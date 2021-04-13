6f4c820d167405752eb606f9dea1d661aedaa81e
Zych Grzegorz <gzych@kbj.com.pl>
2021-04-13 13:56:47 +0200

    nested views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,26 +1,11 @@
 sap.ui.define(
   [
     'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast',
-    'sap/ui/model/json/JSONModel',
-    'sap/ui/model/resource/ResourceModel',
   ],
-  function (Controller, MessageToast, JSONModel, ResourceModel) {
+  function (Controller) {
     'use strict'
-    //controla widoku
-    //ogarnia to co zwiaane z widokiem
-    return Controller.extend('sap.ui.demo.walkthrough.App', {
 
-      onShowHello: function () {
-        //read msg from i18n model
-        let oBundle = this.getView().getModel('i18n').getResourceBundle()
-        let sRecipient = this.getView()
-          .getModel()
-          .getProperty('/recipient/name')
-        let sMsg = oBundle.getText('helloMsg', [sRecipient])
-      
-        MessageToast.show(sMsg)
-      },
+    return Controller.extend('sap.ui.demo.walkthrough.App', {
     })
   }
 )
